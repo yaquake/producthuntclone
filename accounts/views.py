f54d03610c968a42f8eaa0d2494c4e9f9d0e527e
yaquake <ivan@programmer.net>
2018-05-21 23:02:21 +1200

    compass png

--- accounts/views.py
+++ accounts/views.py
@@ -7,13 +7,17 @@ def signup(request):
     if request.method == 'POST':
         #   Button pressed
         if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-                auth.login(request, user)
-                return redirect('home')
+            if 8 <= len(request.POST['password1']) <= 20:
+                try:
+                    user = User.objects.get(username=request.POST['username'])
+                    return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+                except User.DoesNotExist:
+                    user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                    auth.login(request, user)
+                    return redirect('home')
+            else:
+                return render(request, 'accounts/signup.html', {'error': 'Password must be between 8 and 20 letters.'})
+
         else:
             return render(request, 'accounts/signup.html', {'error': 'Passwords must match'})
 
